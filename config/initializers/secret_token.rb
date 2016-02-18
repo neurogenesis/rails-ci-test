8e99dc7f84929fbffa0bf5313358ee04cfc2df16
Ives Stoddard <Ives Stoddard>
2016-02-18 14:18:15 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rover::Application.config.secret_key_base = '72bce1fe70c61e4496e672dbafdf85e52188e449f0b976e14e4340af88c549a5f6370c5e43151e390c61d1bada8360bfc428ef0f264b4c80a30e681d88cea0b9'
